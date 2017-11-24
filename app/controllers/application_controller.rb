15d736a6755fe5a6199caea364a8a7d279cf798c
Randall Robinson <keith_adams007@yahoo.com>
2017-11-24 19:56:43 +0000

    set up appcontroller configure and helpers

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+require './config/environment'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base
+  use Rack::Flash
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+
+
+
+
+
+    helpers do
+      def logged_in?
+        !!current_user
+      end
+
+      def current_user
+        @current_user ||= User.find(session[:id]) if session[:id]
+      end
+    end
+  end
