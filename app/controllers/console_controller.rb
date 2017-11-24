c21dc8377f4f42d02061896d3eacfaab08271b9a
Randall Robinson <keith_adams007@yahoo.com>
2017-11-24 21:27:52 +0000

    consoles new added

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -19,7 +19,24 @@ class ConsoleController < Sinatra::Base
     end
   end
 
+  get '/consoles/new' do
+    if logged_in?
+      erb :'consoles/new'
+    else
+      redirect '/login'
+    end
+  end
 
+  post '/consoles' do
+    @console = Console.new(name: params[:name], user_id: session[:id])
+    if @console.valid?
+      @console.save
+      redirect "/consoles"
+    else
+      flash[:message] = "Error! Console must have a name!"
+      redirect '/consoles/new'
+    end
+  end
 
 
 
