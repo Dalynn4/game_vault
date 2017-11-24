a651f6ba85c74e8f32f218ed3571090f28dfc435
Randall Robinson <keith_adams007@yahoo.com>
2017-11-24 21:21:40 +0000

    get consoles index added

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -2,4 +2,36 @@ require './config/environment'
 require 'rack-flash'
 
 class ConsoleController < Sinatra::Base
+  use Rack::Flash
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/consoles' do
+    if logged_in?
+      erb :'consoles/index'
+    else
+      redirect '/login'
+    end
+  end
+
+
+
+
+
+
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:id]) if session[:id]
+    end
+  end
 end
